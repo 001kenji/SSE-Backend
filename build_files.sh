c1f6c3340a5ece4d37a95f4d995c2c7103c98bf0
BrianNjuguna <142185778+001kenji@users.noreply.github.com>
2024-05-25 13:23:53 +0300

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,5 @@
-pip install -r requirements.txt
+#!/usr/bin/env bash
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
 python3.9 manage.py collectstatic --noinput
